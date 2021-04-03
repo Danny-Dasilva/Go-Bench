ebb4ce3ca49d4b5ad93fb0aac32a8ea6821ef620
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-04-03 11:49:00 -0400

    set for testing

--- Requests/fast_concurrent.go
+++ Requests/fast_concurrent.go
@@ -1,68 +1,68 @@
-package main
+// package main
 
-import (
-	//"encoding/json"
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"sync"
-	"time"
+// import (
+// 	//"encoding/json"
+// 	"bytes"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"sync"
+// 	"time"
 
-	"github.com/valyala/fasthttp"
-	//"github.com/valyala/fastjson"
-)
+// 	"github.com/valyala/fasthttp"
+// 	//"github.com/valyala/fastjson"
+// )
 
-//type Comic struct {
-//Num   int    `json:"num"`
-//Link  string `json:"link"`
-//Img   string `json:"img"`
-//Title string `json:"title"`
-//}
+// //type Comic struct {
+// //Num   int    `json:"num"`
+// //Link  string `json:"link"`
+// //Img   string `json:"img"`
+// //Title string `json:"title"`
+// //}
 
-const baseURL = "http://localhost:8080"
+// const baseURL = "http://localhost:8080"
 
-func getComic(comicID int) (comic string, err error) {
-    _, body, err  := fasthttp.Get(nil, baseURL)
-    if err != nil {
-        return "aa", err
-    }
-    //t := json.Unmarshal(body, &dat) 
+// func getComic(comicID int) (comic string, err error) {
+//     _, body, err  := fasthttp.Get(nil, baseURL)
+//     if err != nil {
+//         return "aa", err
+//     }
+//     //t := json.Unmarshal(body, &dat) 
 
-    bodyBytes, err := ioutil.ReadAll(bytes.NewBuffer(body))
+//     bodyBytes, err := ioutil.ReadAll(bytes.NewBuffer(body))
 
-    //ioutil.ReadAll(resp.Body)
-    //err = json.NewDecoder(decoded).Decode(&comic)
-    //if err != nil {
-        //return nil, err
-    //}
+//     //ioutil.ReadAll(resp.Body)
+//     //err = json.NewDecoder(decoded).Decode(&comic)
+//     //if err != nil {
+//         //return nil, err
+//     //}
 
-    comic = string(bodyBytes) 
-    return comic, nil
-}
+//     comic = string(bodyBytes) 
+//     return comic, nil
+// }
 
-func main() {
-    start := time.Now()
-    defer func() {
-        fmt.Println("Execution Time: ", time.Since(start))
-    }()
+// func main() {
+//     start := time.Now()
+//     defer func() {
+//         fmt.Println("Execution Time: ", time.Since(start))
+//     }()
 
-    //comicsNeeded := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-    wg := sync.WaitGroup{}
+//     //comicsNeeded := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+//     wg := sync.WaitGroup{}
 
-    for i := 1; i <= 500; i++ {
-        wg.Add(1)
-        go func(i int) {
-            _, err := getComic(i)
+//     for i := 1; i <= 1000; i++ {
+//         wg.Add(1)
+//         go func(i int) {
+//             _, err := getComic(i)
 
-            if err != nil {
-                return
-            }
+//             if err != nil {
+//                 return
+//             }
 
-            //fmt.Println("running", i, "\n")
-            //fmt.Printf("Fetched comic %d with title %v\n", i, comic)
-            wg.Done()
-        }(i)
-    }
+//             fmt.Println("running", i, "\n")
+//             //fmt.Printf("Fetched comic %d with title %v\n", i, comic)
+//             wg.Done()
+//         }(i)
+//     }
 
-    wg.Wait()
-}
+//     wg.Wait()
+// }
--- Requests/nonconcurrent.go
+++ Requests/nonconcurrent.go
@@ -1,9 +1,13 @@
-package nonconcurrent 
+package main 
 import (
-    "net/http"
-    "fmt"
-    "time"
-    "encoding/json"
+	//"encoding/json"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/valyala/fasthttp"
+	//"github.com/valyala/fastjson"
 )
 type Comic struct {
     Num   int    `json:"num"`
@@ -12,20 +16,17 @@ type Comic struct {
     Title string `json:"title"`
 }
 
-const baseXkcdURL = "https://xkcd.com/%d/info.0.json"
+const baseURL = "http://localhost:8080"
 
-func getComic(comicID int) (comic *Comic, err error) {
-    url := fmt.Sprintf(baseXkcdURL, comicID)
-    response, err := http.Get(url)
+func getComic(comicID int) (comic string, err error) {
+    _, body, err  := fasthttp.Get(nil, baseURL)
     if err != nil {
-        return nil, err
+        return "aa", err
     }
 
-    err = json.NewDecoder(response.Body).Decode(&comic)
-    if err != nil {
-        return nil, err
-    }
+    bodyBytes, err := ioutil.ReadAll(bytes.NewBuffer(body))
 
+    comic = string(bodyBytes) 
     return comic, nil
 }
 
@@ -35,15 +36,14 @@ func main() {
         fmt.Println("Execution Time: ", time.Since(start))
     }()
 
-    comicsNeeded := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-    comicMap := make(map[int]*Comic, len(comicsNeeded))
+  
 
-    for _, id := range comicsNeeded {
-        comic, err := getComic(id)
+    for i := 1; i <= 10000; i++ {
+        _, err := getComic(i)
         if err != nil {
       continue
         }
-    comicMap[id] = comic
-        fmt.Printf("Fetched comic %d with title %v\n", id, comic.Title)
+
+        fmt.Println("running", i, "\n")
     }
 }
